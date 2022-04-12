4d41f0254b559d9fd3638b8fa9b135cf0edd64f0
nathalieleave <nathalieleave@ufl.edu>
2022-04-12 15:33:59 -0400

    font change

--- src/background.js
+++ src/background.js
@@ -4,3 +4,9 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log("Default background color set to %cgreen", `color: ${color}`);
 });
+
+/*
+  chrome.fontSettings.setFont(
+  { genericFamily: 'sansserif', script: 'Zyyy', fontId: 'MS PGothic' }
+  );
+  */
