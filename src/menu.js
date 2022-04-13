f949655d9ab320527816eb89cc89fde7eef40bef
unknown <aayansiddiqui@ufl.edu>
2022-04-13 19:07:16 -0400

    button changes and UI-feature interactions

--- src/menu.js
+++ src/menu.js
@@ -20,38 +20,37 @@ function handleButtonClick() {
 
 //TEST: handleButtonClick should return the userinput from chrome storage
 function handleButtonClick() {
-    // Use stored sync value.
-    chrome.storage.sync.get("title", ({ title }) => {
-      //alert(title);
-    });
-    chrome.storage.sync.get("url", ({ url }) => {
-      //alert(url);
-    });
-    chrome.storage.sync.get("summary", ({ summary }) => {
-      //alert(summary);
-    });
-  }
+  // Use stored sync value.
+  chrome.storage.sync.get("title", ({ title }) => {
+    //alert(title);
+  });
+  chrome.storage.sync.get("url", ({ url }) => {
+    //alert(url);
+  });
+  chrome.storage.sync.get("summary", ({ summary }) => {
+    //alert(summary);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", documentEvents2, false);
+function documentEvents2() {
   
-  document.addEventListener("DOMContentLoaded", documentEvents2, false);
-  function documentEvents2() {
-    
-    document.getElementById("save").addEventListener("click", async () => {
-      let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-      // Store sync value before the script is executed.
-      let title = document.getElementById("title").value;
-      chrome.storage.sync.set({ 'titleKey' : title}, function(){});
-      let url = document.getElementById("url").value;
-      chrome.storage.sync.set({ url });
-      let summary = document.getElementById("summary").value;
-      chrome.storage.sync.set({ summary });
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: handleButtonClick,
-      });
-      chrome.storage.sync.get(['titleKey'], function(result){
-        alert(result.titleKey);
-      });
-      window.location.href = "popup.html"; //opens new page after data is stored
+  document.getElementById("save").addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    // Store sync value before the script is executed.
+    let title = document.getElementById("title").value;
+    chrome.storage.sync.set({ 'titleKey' : title}, function(){});
+    let url = document.getElementById("url").value;
+    chrome.storage.sync.set({ url });
+    let summary = document.getElementById("summary").value;
+    chrome.storage.sync.set({ summary });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: handleButtonClick,
     });
-  }
-  
+    chrome.storage.sync.get(['titleKey'], function(result){
+      alert(result.titleKey);
+    });
+    window.location.href = "popup.html"; //opens new page after data is stored
+  });
+}
--- src/popup.js
+++ src/popup.js
@@ -11,41 +11,9 @@ function newFileFunction(){
   document.getElementById("New File").addEventListener("click", async () =>{
     let[tab] = await chrome.tabs.query({active:true, currentWindow: true});
     window.location.href = "menu.html";
-//TEST: handleButtonClick should return the userinput from chrome storage
-function handleButtonClick() {
-  // Use stored sync value.
-  chrome.storage.sync.get("title", ({ title }) => {
-    alert(title);
-  });
-  chrome.storage.sync.get("url", ({ url }) => {
-    alert(url);
-  });
-  chrome.storage.sync.get("summary", ({ summary }) => {
-    alert(summary);
   });
 }
 document.addEventListener("DOMContentLoaded", newFileFunction, false);
 
 //export default handleButtonClick;
 //module.exports = handleButtonClick;
-
-/*
-var context_id = -1;
-
-chrome.input.ime.onFocus.addListener(function(context) {
-  context_id = context.contextID;
-});
-
-chrome.input.ime.onKeyEvent.addListener(
-  function(engineID, keyData) {
-    if (keyData.type == "keydown" && keyData.key.match(/^[a-z]$/)) {
-      chrome.input.ime.commitText({"contextID": context_id,
-                                    "text": keyData.key.toUpperCase()});
-      return true;
-    } else {
-      return false;
-    }
-  }
-  
-);
-*/
