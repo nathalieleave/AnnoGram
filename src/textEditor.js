d783e56d19962db92681d361806486dbfeb592ea
lisareichelson <lisareichelson@ufl.edu>
2022-04-18 00:02:27 -0400

    DONE!!!

--- src/textEditor.js
+++ src/textEditor.js
@@ -1,32 +1,32 @@
-
-chrome.storage.sync.get(['File_Name'], function(result){
+chrome.storage.sync.get(["File_Name"], function (result) {
   let stringVar = result.File_Name;
-  if(stringVar == "titleKey1"){
-    chrome.storage.sync.get(["titleKey1"], function(result1){
+  if (stringVar == "titleKey1") {
+    chrome.storage.sync.get(["titleKey1"], function (result1) {
       document.getElementById("FileName").innerHTML = result1.titleKey1;
-    })
-  }
-  else if(stringVar == "titleKey2"){
-    chrome.storage.sync.get(["titleKey2"], function(result1){
+    });
+  } else if (stringVar == "titleKey2") {
+    chrome.storage.sync.get(["titleKey2"], function (result1) {
       document.getElementById("FileName").innerHTML = result1.titleKey2;
-    })
-  }
-  else{
-    chrome.storage.sync.get(["titleKey3"], function(result1){
+    });
+  } else {
+    chrome.storage.sync.get(["titleKey3"], function (result1) {
       document.getElementById("FileName").innerHTML = result1.titleKey3;
-    })
+    });
   }
 });
-
 
 document.getElementById("subnote").addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   let note = document.getElementById("addTxt").value;
   chrome.storage.sync.set({ note: note }, function () {});
-  chrome.storage.sync.get("note", ({ note }) => {
-    alert(note);
-  });
+  //chrome.storage.sync.get("note", ({ note }) => {
+  // alert(note);
+  //});
   document.getElementById("currentNotes").innerHTML = note;
+});
+
+chrome.storage.sync.get(["url"], function (result) {
+  document.getElementById("link1").innerHTML = result.url;
 });
 
 /*chrome.storage.sync.get(["note"], function (result) {
